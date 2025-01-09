084ee24ab2148f5ace53c8b2cc7e65d200e409a7
tsantosale <tsantos.ale@gmail.com>
2025-01-09 13:36:36 -0300

    alterações saída

--- desafiologica.js
+++ desafiologica.js
@@ -2,24 +2,24 @@ let nomeHeroi = "Gretta"
 let XPHeroi = 5000
 
 if(XPHeroi <1000){
-  console.log("Classifição da heroina " + nomeHeroi + " é Ferro");
+  console.log("A heroína é " + nomeHeroi + " do nível Ferro");
 } else if ((XPHeroi >1000) && (XPHeroi <=2000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Bronze");
+  console.log ("A heroína é " + nomeHeroi + " do nível Bronze");
 } else if ((XPHeroi >2000) && (XPHeroi <=5000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Prata");
+  console.log ("A heroína é " + nomeHeroi + " do nível Prata");
 }
 else if ((XPHeroi >5000) && (XPHeroi <=7000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Ouro");
+  console.log ("A heroína é " + nomeHeroi + " do nível Ouro");
 }
 else if ((XPHeroi >7000) && (XPHeroi <=8000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Platina");
+  console.log ("A heroína é " + nomeHeroi + " do nível Platina");
 }
 else if ((XPHeroi >8000) && (XPHeroi <=9000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Ascendente");
+  console.log ("A heroína é " + nomeHeroi + " do nível Ascendente");
 }
 else if ((XPHeroi >9000) && (XPHeroi <=10000)) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Imortal");
+  console.log ("A heroína é " + nomeHeroi + " do nível Imortal");
 }
 else if (XPHeroi >10000) {
-  console.log ("Classifição da heroina " + nomeHeroi + " é Imortal");
+  console.log ("A heroína é " + nomeHeroi + " do nível Imortal");
 }
