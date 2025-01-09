2e51810c1a146f952d4be38d5553f3084f56fafd
tsantosale <tsantos.ale@gmail.com>
2025-01-09 13:28:48 -0300

    consegui

--- desafiologica.js
+++ desafiologica.js
@@ -1,8 +1,25 @@
 let nomeHeroi = "Gretta"
 let XPHeroi = 5000
 
-if(XPHeroi <=1000){
+if(XPHeroi <1000){
   console.log("Classifição da heroina " + nomeHeroi + " é Ferro");
-} else if (XPHeroi <=2002) {
+} else if ((XPHeroi >1000) && (XPHeroi <=2000)) {
   console.log ("Classifição da heroina " + nomeHeroi + " é Bronze");
+} else if ((XPHeroi >2000) && (XPHeroi <=5000)) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Prata");
+}
+else if ((XPHeroi >5000) && (XPHeroi <=7000)) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Ouro");
+}
+else if ((XPHeroi >7000) && (XPHeroi <=8000)) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Platina");
+}
+else if ((XPHeroi >8000) && (XPHeroi <=9000)) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Ascendente");
+}
+else if ((XPHeroi >9000) && (XPHeroi <=10000)) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Imortal");
+}
+else if (XPHeroi >10000) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Imortal");
 }
