c6bf48aaac4f14204aa2fe35027605ae5bb008da
tsantosale <tsantos.ale@gmail.com>
2025-01-09 14:10:48 -0300

    limpando

--- outraformaderesolver.js
+++
@@ -1,8 +0,0 @@
-let nomeHeroi = "Gretta"
-let XPHeroi = 5000
-let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
-
-switch (XPHeroi){
-    case (XPHeroi <1000):
-    console.log("A heroína " + nomeHeroi + " do nível " + nivel[0])
-}
