df306ae8489947bd7d06831bdf757fbcee796e8b
tsantosale <tsantos.ale@gmail.com>
2025-01-09 14:08:51 -0300

    com a matriz melhorou

--- desafiologica.js
+++ desafiologica.js
@@ -1,25 +1,21 @@
 let nomeHeroi = "Gretta"
 let XPHeroi = 5000
+let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
 
 if(XPHeroi <1000){
-  console.log("A heroína é " + nomeHeroi + " do nível Ferro");
+  console.log("A heroína é " + nomeHeroi + " do nível " + + nivel[0]);
 } else if ((XPHeroi >1000) && (XPHeroi <=2000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Bronze");
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[1]);
 } else if ((XPHeroi >2000) && (XPHeroi <=5000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Prata");
-}
-else if ((XPHeroi >5000) && (XPHeroi <=7000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Ouro");
-}
-else if ((XPHeroi >7000) && (XPHeroi <=8000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Platina");
-}
-else if ((XPHeroi >8000) && (XPHeroi <=9000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Ascendente");
-}
-else if ((XPHeroi >9000) && (XPHeroi <=10000)) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Imortal");
-}
-else if (XPHeroi >10000) {
-  console.log ("A heroína é " + nomeHeroi + " do nível Imortal");
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[2]);
+} else if ((XPHeroi >5000) && (XPHeroi <=7000)) {
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[3]);
+} else if ((XPHeroi >7000) && (XPHeroi <=8000)) {
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[4]);
+} else if ((XPHeroi >8000) && (XPHeroi <=9000)) {
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[5]);
+} else if ((XPHeroi >9000) && (XPHeroi <=10000)) {
+  console.log ("A heroína é " + nomeHeroi + " do nível " + nivel[6]);
+} else if (XPHeroi >10000) {
+  console.log ("A heroína é " + nomeHeroi + " do nível " + + nivel[7]);
 }
---
+++ outraformaderesolver.js
@@ -0,0 +1,8 @@
+let nomeHeroi = "Gretta"
+let XPHeroi = 5000
+let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+switch (XPHeroi){
+    case (XPHeroi <1000):
+    console.log("A heroína " + nomeHeroi + " do nível " + nivel[0])
+}
