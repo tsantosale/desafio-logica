8605b0f5a6c086c6682bfefcb99a34de77fa1075
tsantosale <tsantos.ale@gmail.com>
2025-01-09 11:54:56 -0300

    usando if

--- desafiologica.js
+++ desafiologica.js
@@ -1,1 +1,8 @@
-console.log("Tentando de novo")
+let nomeHeroi = "Gretta"
+let XPHeroi = 5000
+
+if(XPHeroi <=1000){
+  console.log("Classifição da heroina " + nomeHeroi + " é Ferro");
+} else if (XPHeroi <=2002) {
+  console.log ("Classifição da heroina " + nomeHeroi + " é Bronze");
+}
