ac5c1721a520f6fcda1f648920914e7603c8c213
tsantosale <tsantos.ale@gmail.com>
2025-01-11 16:36:14 -0300

    2

--- desafiologica2.js
+++ desafiologica2.js
@@ -1,1 +1,11 @@
-console.log("bora resolver mais um")
+// Primeira parte do desafio 
+// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+function calculoSaldoRankeadas(vitorias, derrotas){
+    return vitorias - derrotas
+   }
+
+let saldoVitorias = calculoSaldoRankeadas(45, 30)
+
+console.log ("O saldo de vitória é " + saldoVitorias) 
