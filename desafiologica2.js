da96f0f49899e95509dfc02030fd4c45b2ac62d8
tsantosale <tsantos.ale@gmail.com>
2025-01-13 08:00:59 -0300

    novo desafio

--- desafiologica2.js
+++ desafiologica2.js
@@ -1,12 +1,12 @@
 // Primeira parte do desafio 
-// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+// Crie função com o parâmetro = quantidade de vitórias e derrotas de um jogador;
+// retorne o resultado para uma variável, o saldo de Rankeadas = (vitórias - derrotas)
 
 function calculoSaldoRankeadas(vitorias, derrotas){
     return vitorias - derrotas
    }
 
-let saldoVitorias = calculoSaldoRankeadas(45, 30)
+let saldoVitorias = calculoSaldoRankeadas(210, 30)
 
 // Segunda parte do desafio
 // Ao final deve se exibir uma mensagem:
@@ -14,9 +14,19 @@ let saldoVitorias = calculoSaldoRankeadas(45, 30)
 
 let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
 
-if (saldoVitorias >=10){
+if (saldoVitorias <=10){
     console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[0]}`)
 }   else if((saldoVitorias >10) && (saldoVitorias <= 20)) {
     console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[1]}`)
+}   else if((saldoVitorias >20) && (saldoVitorias <= 50)) {
+    console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[2]}`)
+}   else if((saldoVitorias >50) && (saldoVitorias <= 80)) {
+    console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[3]}`)
+}   else if((saldoVitorias >80) && (saldoVitorias <= 90)) {
+    console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[4]}`)
+}   else if((saldoVitorias >90) && (saldoVitorias <= 100)) {
+    console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[5]}`)
+}   else if(saldoVitorias >100) {
+    console.log(`A Heroína tem saldo ${saldoVitorias} e está no nível ${nivel[6]}`)
 }
 
